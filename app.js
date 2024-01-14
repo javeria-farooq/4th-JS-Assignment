ccc1864fba2aeb118befda9dfcec4e6dd1801d25
javeria-farooq <gettingeducation6@gmail.com>
2024-01-14 20:23:06 +0500

    4th JS Assignment

---
+++ app.js
@@ -0,0 +1,101 @@
+// alert('Your form has been submitted')
+
+// let alert_var = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dicta illum molestiae maiores voluptates voluptateculpa necessitatibus illo eius. Voluptate similique suscipit dicta natus asperiores illo minima neque deserunt totam."
+
+// document.write(alert_var)
+// document.write(alert_var)
+// document.write(alert_var)
+// document.write(alert_var)
+
+// alert(alert_var)
+// alert(alert_var)
+// alert(alert_var)
+
+                        // NaN (result will be shown as not a nmbr)
+
+// let firstName = 'javeria'
+// let lastName = 'Farooq'
+// document.write(firstName - lastName)
+
+                   // NaN (result will be shown as not a nmbr)
+
+// let a = "2y"
+// let b = 5
+// document.write(a - b)
+
+                //    result will be  shown as "-3"
+
+// let a = "2"
+// let b = 5
+// document.write(a - b)
+
+                //  concate this and result will be shown as javeriafarooq
+
+// let a = "javeria"
+// let b = "farooq"
+// document.write(a + b)
+
+                // NaN (result will be shown as not a nmbr)
+
+// let a = "javeria"
+// let b = "farooq"
+// document.write(a - b)
+
+                // concate this and result will be shown as 12
+
+// let a = "1"
+// let b = "2"
+// document.write(a + b)
+
+                // add this and result will be shown as 12
+
+// let a = 1
+// let b = "2"
+// document.write(a + b)
+
+                // add this and result will be shown as 3
+
+// let a = 1
+// let b = 2
+// document.write(a + b)
+
+                // subtract this and result will be shown as -1
+
+// let a = 1
+// let b = 2
+// document.write(a - b)
+
+                 // subtract this and result will be shown as -1
+
+// let a = "1"
+// let b = "2"
+// document.write(a - b)
+
+                // subtract this and result will be shown as -1
+
+// let a = "1"
+// let b = 2
+// document.write(a - b)
+
+                // subtract this and result will be shown as -1
+
+// let a = 1
+// let b = 2
+// document.write(a - b)
+
+                 // multiply this and result will be shown as 4
+
+// let a = "2"
+// let b = "2"
+// document.write(a * b)
+
+// let user_message1 = prompt("Enter name")
+// let user_message2 = prompt("Enter age")
+// let add = user_message1 + user_message2
+// document.write(add)
+
+// let a = +prompt('enter name')
+// document.write(a)
+// document.write(typeof(a))
+
+// document.write(user_message)
